ad2fcfcadd8f2f978f0189089faa695634dce096
Wajiha Ahamed Warka <49165042+wajiha07@users.noreply.github.com>
2021-12-26 18:24:09 +0600

    Add files via upload

---
+++ src/Context/GlobalState.js
@@ -0,0 +1,43 @@
+import React, {createContext, useReducer} from "react";
+import AppReducer from './AppReducer';
+
+//Initial state
+const initialState ={
+    transactions: [ ]
+}
+//create contrxt
+const GlobalContext = createContext(initialState);
+
+//Provider Component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+   
+
+    //Action
+    function deleteTransaction(id){
+       dispatch({
+           type: 'DELETE_TRANSACTION',
+            payload: id 
+       });  
+    }
+
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+             payload: transaction
+        });  
+     }
+
+
+    return (<GlobalContext.Provider value={{ 
+        transactions:state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>
+
+        {children}
+     </GlobalContext.Provider>);
+}
+
+
+export default GlobalContext;
